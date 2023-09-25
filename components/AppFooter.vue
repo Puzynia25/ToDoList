e5812be39e67c4e2c468d3914702a6ebff43c34f
Vitalina Puzynia <102970899+Puzynia25@users.noreply.github.com>
2023-09-25 17:27:38 +0200

    First version

---
+++ components/AppFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer :todos="todos" class="text-sm text-gray-300 mt-10">
+    {{ todos.length }} more to do, {{ filteredDone }} done
+  </footer>
+</template>
+
+<script lang="ts">
+import { Todo } from '~/types/Todo';
+
+export default defineComponent({
+  props: {
+    todos: {
+      type: Array as PropType<Todo[]>,
+      required: true,
+    },
+  },
+  computed: {
+    filteredDone(): number {
+      const result = this.todos.filter((todo) => todo.completed);
+      return result.length;
+    },
+  },
+});
+</script>
